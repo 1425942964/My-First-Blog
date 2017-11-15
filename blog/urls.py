184cbcb0274ff16673a6b22942a58633b17de1e6
Andy Cao <andycao1998@hotmail.com>
2017-11-14 21:12:06 -0500

    Modified website with buttons and display posts.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.introduction, name='introduction'),
+	url(r'^posts/', views.post_list, name='posts'),
+	url(r'^contact/', views.contact, name='contact')
 ]
