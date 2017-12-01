dfcd8aeb059083c670b417025088c763623792a2
Andy Cao <andycao1998@gmail.com>
2017-12-01 18:52:23 -0500

    wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = '/home/yourusername/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
 application = get_wsgi_application()
